310be7a7e4ac558a4e0aeee83c494ef8d612e448
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-03-17 20:21:36 -0400

    Update structure

---
+++ app/controllers/test.rb
@@ -0,0 +1,53 @@
+# endpoint related to test interface
+
+class TestController < Sinatra::Base
+	# Test: reset and add n random test users and t tweets
+	get '/test/reset?users=n&tweets=t' do
+		# load seed data
+		load './db/seeds.rb'
+		count = 0
+		n = params[:n]
+		t = params[:t]
+		users = []
+		tweets = []
+		follows = []
+		while count < n
+			# randomly select a user
+			user = User.all.sample()
+			# get all follows of the randomly selected user
+			follow = Follow.all.select{ |f|
+				f["fan_id"] == user["id"] or f["idol_id"] == user["id"]
+			}
+			# get all tweets of the randomly selected user
+			tweet = Tweet.all.select{ |t|
+				t["user_id"] == user["id"]
+			}
+			if !users.include?(user) and tweet.length >= t
+				users << user
+				tweets << tweet
+				follows << follow
+				count += 1
+		end
+		@users = users.to_json
+		@tweets = tweets.to_json
+		status 200
+	end
+
+	# Test: get y random tweets of test user id x
+	get '/test/tweet?user_id=x&tweet_count=y' do
+		x = params[:x]
+		y = params[:y]
+		user = User.find_by_id(x)
+		tweets = Tweet.all.select{ |t|
+				t["user_id"] == user["id"]
+			}
+		end
+		@user = user.to_json
+		@tweets = tweets.sample(y).to_json
+		status 200
+	end
+
+	# Test: check validation
+	# get '/test/validate?n=n' do
+	# end
+end
---
+++ app/controllers/timeline.rb
@@ -0,0 +1,15 @@
+# endpoint related to test timeline
+
+class Timeline < Sinatra::Base
+	# Get y latest tweets of particular user x
+	get '/users/tweet?user_id=x&tweet_count=y' do
+		# find all tweets of a user with id x and sort by created_at in reverse chronological order
+		tweets = Tweet.where(user_id: params[:x]).order(created_at: :desc)
+		if tweets.empty?
+			status 401
+		else
+			tweets.first(params[:y]).to_json
+			status 200
+		end
+	end
+end

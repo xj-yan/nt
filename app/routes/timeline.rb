a485e70cc1a4c17c124b018b7871a54d7d1be36c
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-03-19 21:16:51 -0400

    Add route for timeline

--- app/routes/timeline.rb
+++ app/routes/timeline.rb
@@ -1,15 +1,32 @@
 # endpoint related to test timeline
 
 class App < Sinatra::Base
-	# Get y latest tweets of particular user x
-	get '/users/tweet?user_id=x&tweet_count=y' do
-		# find all tweets of a user with id x and sort by created_at in reverse chronological order
-		tweets = Tweet.where(user_id: params[:x]).order(created_at: :desc)
-		if tweets.empty?
-			status 401
-		else
-			tweets.first(params[:y]).to_json
-			status 200
+	
+	helpers do
+		# Get a list of followee id
+		def get_followees(id)
+			followees = Follow.where(fan_id: id)
+		end
+		
+		# Return first 10 tweet where id is in the given
+		# id array in descending order
+		# Action: increase number of tweet display using pagination
+		def get_tweet(ids)
+			tweets = Tweet.where(user_id: ids).order(created_at: :desc).first(10)
 		end
 	end
+
+	# Display home timeline in reverse chronological order 
+	# if id is equal to id in session
+	# Otherwise display timeline of following users'
+	# Action: protected needed
+	get '/users/:id/timeline' do
+		id = params[:x]
+		if id == session[:user_id]
+			get_tweet(id)
+		else
+			get_tweet(get_followees(id))
+		end
+	end
+
 end

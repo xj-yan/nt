d8b2d5006d1e6a9bf42487b617a5d920cdce375b
Xiaojing Yan <xyan@brandeis.edu>
2020-03-26 18:18:44 -0400

    initial repo

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
-ENV["RACK_ENV"] ||= "development"
+ENV["APP_ENV"] ||= "development"
 
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-ENV['RACK_ENV'] ||= "development"
+ENV['APP_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV['APP_ENV'])
 
-ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
+ActiveRecord::Base.establish_connection(ENV['APP_ENV'].to_sym)
 
 require_all 'app'

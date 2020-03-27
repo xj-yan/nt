256cc1bac13b9715f99910227442c75039030b57
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-03-26 22:29:57 -0400

    Update configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,30 @@
-ENV['APP_ENV'] ||= "development"
+
+ENV['RACK_ENV'] ||= "development"
+ENV['DB_HOST'] = "gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com"
+ENV['DB_PASSWORD'] = "iyajy1kgp2nczrpi"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['APP_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'])
 
-ActiveRecord::Base.establish_connection(ENV['APP_ENV'].to_sym)
+ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
 
 require_all 'app'
+
+unless ENV['RACK_ENV'] == 'development'
+	require 'rack-timeout'
+	use Rack::Timeout, service_timeout: 9
+end
+
+# configure :development do
+# #  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')	
+
+# 	ActiveRecord::Base.establish_connection(
+# 		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+# 		:host     => db.host,
+# 		:username => db.user,
+# 		:password => db.password,
+# 		:database => db.path[1..-1],
+# 		:encoding => 'utf8'
+# 	)
+	
+# end

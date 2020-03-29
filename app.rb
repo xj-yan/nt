e9883bcc57ba73d0276a3a4bd6ba52b57db9ab61
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-03-29 10:11:21 -0400

    Update path

--- app.rb
+++ app.rb
@@ -16,6 +16,20 @@ class App < Sinatra::Base
   set :root,                    File.dirname(__FILE__)
   set :views,                   Proc.new { File.join(root, "app/views") }
   set :public_folder,           Proc.new { File.join(root, "public")}
+  set :partial_template_engine, :erb
+
+  enable :logging, :sessions
+
+  configure :staging, :production do
+    set :raise_errors, true
+    set :show_exceptions, false
+    set :dump_errors, false
+
+    # file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+    # file.sync = true
+    # use Rack::CommonLogger, file
+  end
+
 
   run!
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 # using Rack handler, run config.ru command: rackup -p 4567
 # require './app'
-require './config/environment'
+# require './config/environment'
+ENV['ROOT_PATH'] = "#{File.expand_path(File.dirname(__FILE__))}"
+
+require 'config/environment'
 run App

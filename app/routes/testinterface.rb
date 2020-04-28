3a9ea604fd31dcb6315d2a36130bb343ad27e768
Xiaojing Yan <xyan@brandeis.edu>
2020-04-28 09:48:20 -0400

    specify version

--- app/routes/testinterface.rb
+++ app/routes/testinterface.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
 	# end
 
 	get '/test' do
-		system("PGPASSWORD=iyajy1kgp2nczrpi /usr/bin/pg_dump --clean -h gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com -p 25060 -U doadmin -Fc -t users nt_dev > ./lib/backup/user_dump_file.pgsql")
+		system("PGPASSWORD=iyajy1kgp2nczrpi pg_dump --cluster 10.12/main --clean -h gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com -p 25060 -U doadmin -Fc -t users nt_dev > ./lib/backup/user_dump_file.pgsql")
 		# system("PGPASSWORD=iyajy1kgp2nczrpi pg_dump --clean -h gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com -p 25060 -U doadmin -Fc -t follows nt_dev > ./lib/backup/follow_dump_file.pgsql")
 		# system("PGPASSWORD=iyajy1kgp2nczrpi pg_dump --clean -h gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com -p 25060 -U doadmin -Fc -t tweets nt_dev > ./lib/backup/tweet_dump_file.pgsql")
 		# puts "dumped"
@@ -31,7 +31,7 @@ class App < Sinatra::Base
 		# system("psql -U doadmin -h gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com -p 25060")
 		
 		# system("'postgresql://doadmin:iyajy1kgp2nczrpi@gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com:25060/nt_dev?sslmode=require' ")
-		system ("PGPASSWORD=iyajy1kgp2nczrpi /usr/bin/pg_restore -d 'postgresql://doadmin:iyajy1kgp2nczrpi@gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com:25060/nt_dev?sslmode=require' --jobs 4 ./lib/backup/user_dump_file.pgsql")
+		system ("PGPASSWORD=iyajy1kgp2nczrpi pg_restore --cluster 10.12/main -d 'postgresql://doadmin:iyajy1kgp2nczrpi@gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com:25060/nt_dev?sslmode=require' --jobs 4 ./lib/backup/user_dump_file.pgsql")
 		# system ("PGPASSWORD=iyajy1kgp2nczrpi pg_restore -d 'postgresql://doadmin:iyajy1kgp2nczrpi@gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com:25060/nt_dev?sslmode=require' --jobs 4 follow_dump_file.pgsql")
 		# system ("PGPASSWORD=iyajy1kgp2nczrpi pg_restore -d 'postgresql://doadmin:iyajy1kgp2nczrpi@gigatwitter-db-postgresql-do-user-7074878-0.db.ondigitalocean.com:25060/nt_dev?sslmode=require' --jobs 4 tweet_dump_file.pgsql")
 		

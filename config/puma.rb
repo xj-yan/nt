9f53a4654dbc446beb38a6d8f03e70029f95c615
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-05-02 15:05:02 -0400

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,13 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_worker_boot do
+	# Valid on Rails up to 4.1 the initializer method of setting `pool` size
+	ActiveSupport.on_load(:active_record) do
+		config = ActiveRecord::Base.configurations[Rails.env] ||
+								Rails.application.config.database_configuration[Rails.env]
+		config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+		ActiveRecord::Base.establish_connection(config)
+	end
+end

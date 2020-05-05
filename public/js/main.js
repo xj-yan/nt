e1b42cd421f67b7904582236d8dcc61b4f7d0407
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-05-05 00:17:17 -0400

    Update search & main

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,31 @@
-function entersearch() {
-    var event = window.event || arguments.callee.caller.arguments[0];
-    if (event.keyCode == 13) {
-        var query = document.getElementById('search').value;
-        console.log(query);
-        console.log("start searching");
-        window.location.href = '/search?query=' + query;
-    }
-}
+$(document).ready(function() {
+    // Logout
+    $("#logout-btn").click(function() {
+        window.location.href = '/logout';
+    });
+
+    $(".nav-bar-item-wrap").mouseenter(function() {
+        $(this).css({
+            "color": "rgb(29, 161, 242)",
+            "background-color": "rgb(245,248,250)",
+            "border-radius": "25px",
+            "transition": "all 0.5s",
+            "cursor": "pointer"
+        });
+
+        $(this).find("i").css({
+            "color": "rgb(29, 161, 242)",
+            "transition": "all 0.5s"
+        });
+    });
+
+    $(".nav-bar-item-wrap").mouseleave(function() {
+        $(this).css({
+            "color": "black",
+            "background-color": "rgba(245,248,250,0)",
+            "border-radius": "25px"
+        });
+
+        $(this).find("i").css("color", "black");
+    });
+});

fa3a6781c8af9dc9c7f122787095fcd282e82ced
Xiaojing Yan <xyan@brandeis.edu>
2020-05-07 19:05:50 -0400

    update rabbitmq channel name

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ require "bunny"
 conn = Bunny.new("amqp://admin:admin@167.99.1.171:5672")
 conn.start
 ch = conn.create_channel
-$q = ch.queue("bunny.test", :auto_delete => true)
+$q = ch.queue("bunny.test_1", :auto_delete => true)
 $x = ch.default_exchange
 
 

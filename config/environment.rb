7dd55a6d9f22f0c80e47c2bcf469792e8b01ecef
Xiaojing Yan <xyan@brandeis.edu>
2020-05-07 23:59:41 -0400

    turn off queue

--- config/environment.rb
+++ config/environment.rb
@@ -28,12 +28,12 @@ $redis = Redis.new(
 	host: 'localhost'
 )
 
-require "bunny"
-conn = Bunny.new("amqp://admin:admin@167.99.1.171:5672")
-conn.start
-$ch = conn.create_channel
-$q = $ch.queue("bunny.test_1", :auto_delete => true)
-$x = $ch.default_exchange
+# require "bunny"
+# conn = Bunny.new("amqp://admin:admin@167.99.1.171:5672")
+# conn.start
+# $ch = conn.create_channel
+# $q = $ch.queue("bunny.test_1", :auto_delete => true)
+# $x = $ch.default_exchange
 
 
 require_all 'app'

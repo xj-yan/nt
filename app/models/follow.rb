8a380b5b5ecf6696f194152ba180cf6f34626548
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-05-08 15:17:59 -0400

    Update caching procedure

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -7,17 +7,15 @@ class Follow < ActiveRecord::Base
 	:foreign_key => 'followee_id'
 
 	# update cache after query for follow
-	after_find do |user|
-		puts "You have found #{user.followee_id} followed by #{user.follower_id}"
+	# after_find do |user|
+	# 	# puts "You have found #{user.followee_id} followed by #{user.follower_id}"
 		
-		# update cache for user_id/following
-		$redis.SADD("#{user.follower_id}/following", user.followee_id)
-		puts "#{user.followee_id} has added to #{user.follower_id}/following"
-		$redis.expire("#{user.follower_id}/following",15.minute.to_i)
+	# 	# update cache for user_id/following
+	# 	$redis.SADD("#{user.follower_id}/following", user.followee_id)
+	# 	$redis.expire("#{user.follower_id}/following",15.minute.to_i)
 
-		# update cache for user_id/follower
-		$redis.SADD("#{user.followee_id}/follower", user.follower_id)
-		puts "#{user.follower_id} has added to #{user.followee_id}/follower"
-		$redis.expire("#{user.followee_id}/follower",15.minute.to_i)
-	end
+	# 	# update cache for user_id/follower
+	# 	$redis.SADD("#{user.followee_id}/follower", user.follower_id)
+	# 	$redis.expire("#{user.followee_id}/follower",15.minute.to_i)
+	# end
 end

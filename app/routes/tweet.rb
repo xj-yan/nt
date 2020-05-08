8516c3144255c08c5fa62e339e6ca9065d2f60ff
Xiaojing Yan <xyan@brandeis.edu>
2020-05-07 23:51:53 -0400

    rabbitmq add delivery tag

--- app/routes/tweet.rb
+++ app/routes/tweet.rb
@@ -7,21 +7,22 @@ class App < Sinatra::Base
             @user = User.find(session[:user_id])
             response = JSON.parse(request.body.read)
 
-            # task_str = response["tweet"] + ";" + @user.id.to_s
-            # $x.publish(task_str, :routing_key => $q.name)
+            task_str = response["tweet"] + ";" + @user.id.to_s
+            $x.publish(task_str, :routing_key => $q.name)
 
-            # puts task_str
-            # $x.publish(task_str, :routing_key => $q.name)
-            # $q.subscribe(:manual_ack => true) do |delivery_info, metadata, payload|
-            #     puts "Received #{payload}"
-            #     arr = payload.split(";")
-            #     tweet = make_tweet(arr[0], arr[1].to_i)
-            #     tweet.to_json
-            # end
+            puts task_str
+            $x.publish(task_str, :routing_key => $q.name)
+            $q.subscribe(:manual_ack => true) do |delivery_info, metadata, payload|
+                puts "Received #{payload}"
+                arr = payload.split(";")
+                tweet = make_tweet(arr[0], arr[1].to_i)
+                $ch.ack(delivery_info.delivery_tag)
+                tweet.to_json
+            end
 
-            puts response
-            tweet = make_tweet(response["tweet"], @user.id)
-            tweet.to_json
+            # puts response
+            # tweet = make_tweet(response["tweet"], @user.id)
+            # tweet.to_json
         else
             flash[:notice] = "The user doesn't exit."
         end

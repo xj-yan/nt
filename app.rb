d97ee6aed51f7a356de8f260f26ececa8dcf2447
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-05-13 15:39:07 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ class App < Sinatra::Base
   set :root,                    File.dirname(__FILE__)
   set :views,                   Proc.new { File.join(root, "app/views") }
   set :public_folder,           Proc.new { File.join(root, "public")}
-  set :service, ENV['SERVICE_HOST']
-  set :service_port, ENV['SERVICE_PORT'] || 80
+  set :service,                 'http://167.172.148.247/'
+  set :service_port,             80
 
   enable :sessions, :partial_underscores
 

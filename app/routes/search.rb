0d187e1460d9b301b51739332e20a74cea370764
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-05-13 20:16:44 -0400

    Update search.rb

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,13 +1,30 @@
 # endpoint related to search
 require 'json'
+require 'net/http'
 
 class App < Sinatra::Base
 
 	get '/search' do
-		uri = "http://161.35.6.102/search"
-		response = Net::HTTP.post_form(
-		uri, 'query' => params[:query])
-		@res = response
+		q = params[:query]
+		result = $redis.get("search/#{q}")
+		if result.nil?
+			uri = URI("http://161.35.6.102/search")
+			params = {:query => q}
+			uri.query = URI.encode_www_form(params)
+			response = Net::HTTP.get_response(uri)
+			puts response.body if response.is_a?(Net::HTTPSuccess)
+			res = JSON.parse(response.body)
+			if !res.nil?
+				@res = res
+			else
+				@res = []
+			end
+			$redis.set("search/#{q}", @response.body)
+			# Expire the cache, every 15 minutes
+			$redis.expire("search/#{q}", 15.minute.to_i)
+		else
+			@res = JSON.parse(result)
+		end
 		erb :search
 	end
 
